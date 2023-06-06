782984abe651aadadcc829c15fa028c70b016b11
TimaDootaliev <timur.dootaliev.td@gmail.com>
2023-06-06 16:15:42 +0600

    setting up

--- core/settings/settings.py
+++ core/settings/settings.py
@@ -13,9 +13,12 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from datetime import timedelta
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
